f411f08d77fc904bfa04d3280a4ba2cb490dce58
njang <neo.w.jang@gmail.com>
2017-12-19 10:41:56 -0600

    Complete start/stop/reset buttons

--- app.js
+++ app.js
@@ -10,18 +10,39 @@ $(function() {
 // EVENT HANDLERS
 // ==================
 
-var startTimer = function() {
+let timer = 0;
+
+let startTimer = function() {
 	console.log('start timer clicked');
+	timeShown = Number($('#stopwatch').text());
+	timer = setInterval(() => {
+		$('#stopwatch').html(timeShown);
+		timeShown++;
+	}, 1000);
 };
 
 var stopTimer = function() {
-
+	console.log('stop timer clicked');
+	clearInterval(timer);
 };
 
 var resetTimer = function() {
-
+	console.log('reset timer clicked');
+	timeShown = 0;
+	$('#stopwatch').html(timeShown);
+	stopTimer();
 };
 
 var countdownTimer = function () {
-
+	console.log('countdown timer clicked');
+	let countdown = Number($('#stopwatch').text());
+	while (countdown >= 0) {
+		timer = setInterval(() => {
+			$('#stopwatch').html(countdown);
+			countdown--;
+			console.log(countdown);
+		}, 1000);
+	}
 };
+
+
